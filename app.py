4448c5114a65f4376f839416f10380816dad2b32
ajisantso-id <ajisantso11@gmail.com>
2026-03-04 22:02:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -297,12 +297,45 @@ legend_html = '''
     <hr style="margin: 5px 0; border-top: 1px solid #ccc;">
     
     <div style="margin-bottom: 5px; font-size: 11px; color: #333;"><b>Kategori Hujan (24 Jam):</b></div>
-    <div style="margin-bottom: 2px;"><i style="background: blue; border-radius: 50%; width: 12px; height: 12px; float: left; margin-top: 2px; margin-right: 10px;"></i>Cerah (0 mm)</div>
-    <div style="margin-bottom: 2px;"><i style="background: green; border-radius: 50%; width: 12px; height: 12px; float: left; margin-top: 2px; margin-right: 10px;"></i>Ringan (0.1 - 20)</div>
-    <div style="margin-bottom: 2px;"><i style="background: beige; border-radius: 50%; width: 12px; height: 12px; float: left; margin-top: 2px; margin-right: 10px;"></i>Sedang (20 - 50)</div>
-    <div style="margin-bottom: 2px;"><i style="background: orange; border-radius: 50%; width: 12px; height: 12px; float: left; margin-top: 2px; margin-right: 10px;"></i>Waspada (50 - 100)</div>
-    <div style="margin-bottom: 2px;"><i style="background: red; border-radius: 50%; width: 12px; height: 12px; float: left; margin-top: 2px; margin-right: 10px;"></i>Siaga (100 - 150)</div>
-    <div><i style="background: darkred; border-radius: 50%; width: 12px; height: 12px; float: left; margin-top: 2px; margin-right: 10px;"></i>Awas (> 150 mm)</div>
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <div style="margin-bottom: 6px; height: 18px;">
+        <div style="background: blue; border-radius: 50%; width: 18px; height: 18px; color: white; text-align: center; line-height: 18px; float: left; margin-right: 8px; font-size: 10px;">
+            <i class="glyphicon glyphicon-cloud"></i>
+        </div>
+        <span style="line-height: 18px;">Cerah (0 mm)</span>
+    </div>
+    <div style="margin-bottom: 6px; height: 18px;">
+        <div style="background: green; border-radius: 50%; width: 18px; height: 18px; color: white; text-align: center; line-height: 18px; float: left; margin-right: 8px; font-size: 10px;">
+            <i class="glyphicon glyphicon-tint"></i>
+        </div>
+        <span style="line-height: 18px;">Ringan (0.1 - 20)</span>
+    </div>
+    <div style="margin-bottom: 6px; height: 18px;">
+        <div style="background: beige; border-radius: 50%; width: 18px; height: 18px; color: black; text-align: center; line-height: 18px; float: left; margin-right: 8px; font-size: 10px; border: 1px solid #ccc;">
+            <i class="glyphicon glyphicon-tint"></i>
+        </div>
+        <span style="line-height: 18px;">Sedang (20 - 50)</span>
+    </div>
+    <div style="margin-bottom: 6px; height: 18px;">
+        <div style="background: orange; border-radius: 50%; width: 18px; height: 18px; color: white; text-align: center; line-height: 18px; float: left; margin-right: 8px; font-size: 10px;">
+            <i class="glyphicon glyphicon-info-sign"></i>
+        </div>
+        <span style="line-height: 18px;">Waspada (50 - 100)</span>
+    </div>
+    <div style="margin-bottom: 6px; height: 18px;">
+        <div style="background: red; border-radius: 50%; width: 18px; height: 18px; color: white; text-align: center; line-height: 18px; float: left; margin-right: 8px; font-size: 10px;">
+            <i class="glyphicon glyphicon-warning-sign"></i>
+        </div>
+        <span style="line-height: 18px;">Siaga (100 - 150)</span>
+    </div>
+    <div style="margin-bottom: 6px; height: 18px;">
+        <div style="background: darkred; border-radius: 50%; width: 18px; height: 18px; color: white; text-align: center; line-height: 18px; float: left; margin-right: 8px; font-size: 10px;">
+            <i class="glyphicon glyphicon-flash"></i>
+        </div>
+        <span style="line-height: 18px;">Awas (> 150 mm)</span>
+    </div>
 </div>
 '''
 m.get_root().html.add_child(folium.Element(legend_html))
@@ -345,6 +378,7 @@ if data_sensor:
 else:
 
     st.warning("Data API masih kosong / belum ketarik.")
+
 
 
 
