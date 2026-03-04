80c17f1073c4c9a0d275a83f70566382824ffe9d
ajisantso-id <ajisantso11@gmail.com>
2026-03-04 08:56:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -270,7 +270,7 @@ for item in data_sensor:
     except Exception as e:
         continue 
 
-# BIKIN LEGEND MENGAMBANG (UPDATE STANDAR PVMBG)
+# BIKIN LEGEND MENGAMBANG (UPDATE STANDAR PVMBG + BATAS ARG)
 legend_html = '''
 <div style="
     position: fixed; 
@@ -287,14 +287,19 @@ legend_html = '''
     <div style="margin-bottom: 2px;"><i style="background: #ff3385; opacity: 0.6; width: 12px; height: 12px; float: left; margin-right: 8px;"></i>Tinggi</div>
     <div style="margin-bottom: 2px;"><i style="background: #ffff00; opacity: 0.6; width: 12px; height: 12px; float: left; margin-right: 8px;"></i>Menengah</div>
     <div style="margin-bottom: 2px;"><i style="background: #00cc00; opacity: 0.3; width: 12px; height: 12px; float: left; margin-right: 8px;"></i>Rendah</div>
-    <div style="margin-bottom: 8px;"><i style="background: #00ccff; opacity: 0.3; width: 12px; height: 12px; float: left; margin-right: 8px;"></i>Sangat Rendah</div>
+    <div style="margin-bottom: 6px;"><i style="background: #00ccff; opacity: 0.3; width: 12px; height: 12px; float: left; margin-right: 8px;"></i>Sangat Rendah</div>
+    
+    <div style="margin-bottom: 8px;">
+        <hr style="border: none; border-top: 2px dashed black; width: 15px; float: left; margin-top: 6px; margin-right: 8px;">
+        <b>Cakupan Sensor ARG</b>
+    </div>
     
     <hr style="margin: 5px 0; border-top: 1px solid #ccc;">
     
     <div style="margin-bottom: 5px; font-size: 11px; color: #333;"><b>Kategori Hujan (24 Jam):</b></div>
-    <div style="margin-bottom: 2px;"><i style="background: blue; border-radius: 50%; width: 12px; height: 12px; float: left; margin-top: 2px; margin-right: 10px;"></i>Cerah (0 mm)</div>
+    <div style="margin-bottom: 2px;"><i style="background: lightgray; border-radius: 50%; width: 12px; height: 12px; float: left; margin-top: 2px; margin-right: 10px;"></i>Cerah (0 mm)</div>
     <div style="margin-bottom: 2px;"><i style="background: green; border-radius: 50%; width: 12px; height: 12px; float: left; margin-top: 2px; margin-right: 10px;"></i>Ringan (0.1 - 20)</div>
-    <div style="margin-bottom: 2px;"><i style="background: yellow; border-radius: 50%; width: 12px; height: 12px; float: left; margin-top: 2px; margin-right: 10px;"></i>Sedang (20 - 50)</div>
+    <div style="margin-bottom: 2px;"><i style="background: blue; border-radius: 50%; width: 12px; height: 12px; float: left; margin-top: 2px; margin-right: 10px;"></i>Sedang (20 - 50)</div>
     <div style="margin-bottom: 2px;"><i style="background: orange; border-radius: 50%; width: 12px; height: 12px; float: left; margin-top: 2px; margin-right: 10px;"></i>Waspada (50 - 100)</div>
     <div style="margin-bottom: 2px;"><i style="background: red; border-radius: 50%; width: 12px; height: 12px; float: left; margin-top: 2px; margin-right: 10px;"></i>Siaga (100 - 150)</div>
     <div><i style="background: darkred; border-radius: 50%; width: 12px; height: 12px; float: left; margin-top: 2px; margin-right: 10px;"></i>Awas (> 150 mm)</div>
@@ -326,6 +331,7 @@ if data_sensor:
 
         tabel_data.append({
             'Stasiun': item['name_station'],
+            'Kab/Kota': kab_kota,
             'Hujan (mm)': curah,
             'Intensitas': kategori_teks,
             'Status Area': status_teks,
@@ -339,6 +345,7 @@ if data_sensor:
 else:
 
     st.warning("Data API masih kosong / belum ketarik.")
+
 
 
 
